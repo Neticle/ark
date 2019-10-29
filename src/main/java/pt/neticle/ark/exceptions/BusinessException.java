d815844ee905d9cf097e90d72165f6aaffe7861c
Igor Azevedo <igor.azevedo@neticle.pt>
2019-10-29 14:06:27 -0100

    Added BusinessException class

--- src/main/java/pt/neticle/ark/exceptions/BusinessException.java
+++ src/main/java/pt/neticle/ark/exceptions/BusinessException.java
@@ -1,5 +1,18 @@
 package pt.neticle.ark.exceptions;
 
-public class BusinessException
+public class BusinessException extends ArkRuntimeException
 {
+	private final int statusCode;
+
+	public BusinessException (int statusCode, String message)
+	{
+		super(message);
+
+		this.statusCode = statusCode;
+	}
+
+	public int getStatusCode ()
+	{
+		return statusCode;
+	}
 }
